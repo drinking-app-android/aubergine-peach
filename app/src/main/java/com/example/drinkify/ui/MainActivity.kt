4b5d85a626cc677a3b92a8d04c831ed5c90fb2e3
LucasStrand <57836852+LucasStrand@users.noreply.github.com>
2021-02-23 15:52:04 +0100

    Added RandomizeActivity and random apk

---
+++ app/src/main/java/com/example/drinkify/ui/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.drinkify.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.drinkify.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val ranomizeActivityButton: Button = findViewById(R.id.randomizeButton)
+        ranomizeActivityButton.setOnClickListener{
+            val intent = Intent(this, RandomizeActivity::class.java)
+            startActivity(intent)
+        }
+        val generateActivityButton: Button = findViewById(R.id.generateButton)
+        generateActivityButton.setOnClickListener{
+         val intent = Intent(this, GenerateActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
