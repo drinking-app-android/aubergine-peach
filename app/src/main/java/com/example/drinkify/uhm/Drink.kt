594229b4ca5929e68711477dfb467688f2ddf882
Lucas Strand <lucas.strand@outlook.com>
2021-03-05 13:37:26 +0100

    randomize api soon to be done

---
+++ app/src/main/java/com/example/drinkify/uhm/Drink.kt
@@ -0,0 +1,54 @@
+package com.example.drinkify.uhm;
+
+import com.google.gson.annotations.SerializedName
+
+class Drink {
+    //@SerializedName("strDrink")
+    val strDrink: String? = null
+
+    //@SerializedName("strCategory")
+    val strCategory: String? = null
+
+    //@SerializedName("strAlcoholic")
+    val strAlcoholic: String? = null
+
+    //@SerializedName("strInstructions")
+    val strInstructions: String? = null
+/*
+    val strDrinkThumb: String? = null
+    val strIngredient1: String? = null
+    val strIngredient2: String? = null
+    val strIngredient3: String? = null
+    val strIngredient4: String? = null
+    val strIngredient5: String? = null
+    val strIngredient6: String? = null
+    val strIngredient7: String? = null
+    val strIngredient8: String? = null
+    val strIngredient9: String? = null
+    val strIngredient10: String? = null
+    val strIngredient11: String? = null
+    val strIngredient12: String? = null
+    val strIngredient13: String? = null
+    val strIngredient14: String? = null
+    val strIngredient15: String? = null
+    val strMeasure1: String? = null
+    val strMeasure2: String? = null
+    val strMeasure3: String? = null
+    val strMeasure4: String? = null
+    val strMeasure5: String? = null
+    val strMeasure6: String? = null
+    val strMeasure7: String? = null
+    val strMeasure8: String? = null
+    val strMeasure9: String? = null
+    val strMeasure10: String? = null
+    val strMeasure11: String? = null
+    val strMeasure12: String? = null
+    val strMeasure13: String? = null
+    val strMeasure14: String? = null
+    val strMeasure15: String? = null
+
+    @SerializedName("body")
+    val text: String? = null
+*/
+
+}
