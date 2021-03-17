04c387db437023499a87f617419b43f32d0cfb98
GuitarOak <emil.h.karlsson@gmail.com>
2021-03-17 14:34:25 +0100

    Now you can search with multiple ingredients, and also if a drink isnt found an error message will be shown and you can enter new preferences. PreferenceActivity.kt is pretty much done now!

--- app/src/main/java/com/example/drinkify/ui/PreferenceActivity.kt
+++ app/src/main/java/com/example/drinkify/ui/PreferenceActivity.kt
@@ -10,7 +10,10 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.drinkify.R
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.delay
 import org.jetbrains.anko.db.NULL
+import org.jetbrains.anko.doAsync
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -37,7 +40,7 @@ class PreferenceActivity : AppCompatActivity() {
 
         val sweetButton = findViewById<Button>(R.id.sweetButton)
 
-        val sourButton = findViewById<Button>(R.id.sweetButton)
+        val sourButton = findViewById<Button>(R.id.sourButton)
 
         val bitterButton = findViewById<Button>(R.id.bitterButton)
 
@@ -48,21 +51,21 @@ class PreferenceActivity : AppCompatActivity() {
         val fruityButton = findViewById<Button>(R.id.fruityButton)
 
         val veganButton = findViewById<Button>(R.id.veganButton)
-        veganButton.setOnClickListener{
+        veganButton.setOnClickListener {
             veganButton.setBackgroundColor(Color.CYAN)
         }
         val savePreferenceButton = findViewById<Button>(R.id.savePrefButton)
-        savePreferenceButton.setOnClickListener{
+        savePreferenceButton.setOnClickListener {
             TODO("Fixing fundamentals first")
         }
-        vodkaButton.setOnClickListener{
+        vodkaButton.setOnClickListener {
             userChoices[0] = "vodka"
             vodkaButton.setBackgroundColor(Color.CYAN)
             rumButton.setBackgroundColor(Color.DKGRAY)
             tequilaButton.setBackgroundColor(Color.DKGRAY)
             whiskeyButton.setBackgroundColor(Color.DKGRAY)
         }
-        rumButton.setOnClickListener{
+        rumButton.setOnClickListener {
             userChoices[0] = "rum"
 
             rumButton.setBackgroundColor(Color.CYAN)
@@ -70,7 +73,7 @@ class PreferenceActivity : AppCompatActivity() {
             tequilaButton.setBackgroundColor(Color.DKGRAY)
             whiskeyButton.setBackgroundColor(Color.DKGRAY)
         }
-        tequilaButton.setOnClickListener{
+        tequilaButton.setOnClickListener {
             userChoices[0] = "tequila"
 
             tequilaButton.setBackgroundColor(Color.CYAN)
@@ -78,7 +81,7 @@ class PreferenceActivity : AppCompatActivity() {
             vodkaButton.setBackgroundColor(Color.DKGRAY)
             whiskeyButton.setBackgroundColor(Color.DKGRAY)
         }
-        whiskeyButton.setOnClickListener{
+        whiskeyButton.setOnClickListener {
             userChoices[0] = "whiskey"
             whiskeyButton.setBackgroundColor(Color.CYAN)
             vodkaButton.setBackgroundColor(Color.DKGRAY)
@@ -86,28 +89,28 @@ class PreferenceActivity : AppCompatActivity() {
             tequilaButton.setBackgroundColor(Color.DKGRAY)
         }
 
-        sweetButton.setOnClickListener{
+        sweetButton.setOnClickListener {
             userChoices[1] = fruitArray.random()
             sweetButton.setBackgroundColor(Color.CYAN)
             sourButton.setBackgroundColor(Color.DKGRAY)
             bitterButton.setBackgroundColor(Color.DKGRAY)
             saltButton.setBackgroundColor(Color.DKGRAY)
         }
-        sourButton.setOnClickListener{
+        sourButton.setOnClickListener {
             userChoices[1] = "lemon"
             sourButton.setBackgroundColor(Color.CYAN)
             sweetButton.setBackgroundColor(Color.DKGRAY)
             bitterButton.setBackgroundColor(Color.DKGRAY)
             saltButton.setBackgroundColor(Color.DKGRAY)
         }
-        bitterButton.setOnClickListener{
+        bitterButton.setOnClickListener {
             userChoices[1] = "vermouth" //Maybe something else thats bitter
             bitterButton.setBackgroundColor(Color.CYAN)
             sweetButton.setBackgroundColor(Color.DKGRAY)
             sourButton.setBackgroundColor(Color.DKGRAY)
             saltButton.setBackgroundColor(Color.DKGRAY)
         }
-        saltButton.setOnClickListener{
+        saltButton.setOnClickListener {
             userChoices[1] = "salt"
             saltButton.setBackgroundColor(Color.CYAN)
             sourButton.setBackgroundColor(Color.DKGRAY)
@@ -115,12 +118,12 @@ class PreferenceActivity : AppCompatActivity() {
             bitterButton.setBackgroundColor(Color.DKGRAY)
         }
 
-        creamyButton.setOnClickListener{
+        creamyButton.setOnClickListener {
             userChoices[2] = "milk"
             creamyButton.setBackgroundColor(Color.CYAN)
             fruityButton.setBackgroundColor(Color.DKGRAY)
         }
-        fruityButton.setOnClickListener{
+        fruityButton.setOnClickListener {
             userChoices[2] = fruitArray.random()
             fruityButton.setBackgroundColor(Color.CYAN)
             creamyButton.setBackgroundColor(Color.DKGRAY)
@@ -128,87 +131,93 @@ class PreferenceActivity : AppCompatActivity() {
 
         val generateButton = findViewById<Button>(R.id.generatePrefButton)
         generateButton.setOnClickListener {
-
-            val searchResultList: MutableList<String> = ArrayList()
-            var searchURL = ""
-            val retrofit = Retrofit.Builder()
-                    .baseUrl("https://www.thecocktaildb.com/api/json/v2/9973533/")
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-            if (userChoices[3] == "non-alcoholic") {
-                searchURL = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?a=Non_Alcoholic"
-            } else if (userChoices[4] == "shot") {
-                searchURL = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?c=Shot"
-            }else {
-                if (!userChoices[2].isNullOrEmpty() && !userChoices[1].isNullOrEmpty() && !userChoices[0].isNullOrEmpty()){
-                    searchURL = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" + userChoices[0] + "," + userChoices[1] + "," + userChoices[2]
-                }else if (userChoices[2].isNullOrEmpty() && userChoices[1].isNullOrEmpty()) {
-                    searchURL = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" + userChoices[0]
-                }else{
-                    searchURL = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" + userChoices[0] + "," + userChoices[1]
-                }
-            }
-            val searchDrinkByIngredient = retrofit.create(SearchAPI::class.java)
-            searchDrinkByIngredient.list(searchURL)?.enqueue(object : Callback<DrinkHolder> {
-                override fun onResponse(call: Call<DrinkHolder>, response: Response<DrinkHolder>) {
-                    if (!response.isSuccessful) {
-                        Log.d("Response errorBody", response.errorBody().toString())
-                        return
+            if (userChoices[0] == "" && userChoices[1] == "" && userChoices[2] == "" && userChoices[3] == "" && userChoices[4] == ""){
+                val snackbar = Snackbar.make(
+                        findViewById(R.id.prefScrollView),
+                        "You need to atleast one preferences!",
+                        Snackbar.LENGTH_SHORT
+                ).show()
+            } else {
+                val searchResultList: MutableList<String> = ArrayList()
+                var searchURL = ""
+                val retrofit = Retrofit.Builder()
+                        .baseUrl("https://www.thecocktaildb.com/api/json/v2/9973533/")
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+                if (userChoices[3] == "non-alcoholic") {
+                    searchURL = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?a=Non_Alcoholic"
+                } else if (userChoices[4] == "shot") {
+                    searchURL = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?c=Shot"
+                } else {
+                    if (!userChoices[2].isNullOrEmpty() && !userChoices[1].isNullOrEmpty() && !userChoices[0].isNullOrEmpty()) {
+                        searchURL = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" + userChoices[0] + "," + userChoices[1] + "," + userChoices[2]
+                    } else if (!userChoices[1].isNullOrEmpty() && userChoices[0].isNullOrEmpty() && userChoices[2].isNullOrEmpty()) {
+                        searchURL = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" + userChoices[1]
+                    } else if (userChoices[2].isNullOrEmpty() && userChoices[1].isNullOrEmpty()) {
+                        searchURL = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" + userChoices[0]
+                    } else {
+                        searchURL = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" + userChoices[0] + "," + userChoices[1]
                     }
-                    if(response.body()?.drink.isNullOrEmpty()){
-                        Toast.makeText(getApplicationContext(),"No such drink found!",Toast.LENGTH_SHORT).show()
-                    }else{
+                }
+                val searchDrinkByIngredient = retrofit.create(SearchAPI::class.java)
+                searchDrinkByIngredient.list(searchURL)?.enqueue(object : Callback<DrinkHolder> {
+                    override fun onResponse(call: Call<DrinkHolder>, response: Response<DrinkHolder>) {
+                        if (!response.isSuccessful) {
+                            Log.d("Response errorBody", response.errorBody().toString())
+                            return
+                        }
                         val whatsInsideA = response.body()!!
                         for (drinkProperty in whatsInsideA.drink) {
                             searchResultList.add(drinkProperty.idDrink.toString())
                         }
-                        if(searchResultList.isNotEmpty()){
-                            val drinkIDgenerated = searchResultList.random().toInt()
-                            val intent = Intent(this@PreferenceActivity, DrinkActivity::class.java)
-                            intent.putExtra("DrinkID", drinkIDgenerated)
-                            startActivity(intent)
-                        }else{
-                            val intent = Intent(this@PreferenceActivity, DrinkActivity::class.java)
-                            intent.putExtra("DrinkID", 13625)
-                            startActivity(intent)
+                        if (searchResultList.isNotEmpty()) {
+                            if (searchResultList.random().toInt() != null) {
+                                val drinkIDgenerated = searchResultList.random().toInt()
+                                val intent = Intent(this@PreferenceActivity, DrinkActivity::class.java)
+                                intent.putExtra("DrinkID", drinkIDgenerated)
+                                startActivity(intent)
+                                finish()
+                            }
                         }
                     }
-                }
-                override fun onFailure(call: Call<DrinkHolder>, t: Throwable) {
-                    val myToast: Toast = Toast.makeText(this@PreferenceActivity, "No such drink!", Toast.LENGTH_LONG)
-                    myToast.show()
-                }
 
-            })
+                    override fun onFailure(call: Call<DrinkHolder>, t: Throwable) {
+                        val waitTime = 1500
+                        val snackbar = Snackbar.make(
+                                findViewById(R.id.prefScrollView),
+                                "No drink found",
+                                Snackbar.LENGTH_SHORT
+                        ).show()
+                        doAsync {
+                            this@PreferenceActivity
 
-            searchResultList.toList()
+                            Thread.sleep(waitTime.toLong())
+                            startActivity(Intent(this@PreferenceActivity, PreferenceActivity::class.java))
+                        }
 
 
+                    }
+
+                })
+
+            }
         }
 
-
-        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener
-        {
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                if (progress>0 && progress<=25)
-                {
+                if (progress > 0 && progress <= 25) {
                     textView.setText("None")
                     userChoices[3] = "non-alcoholic"
-                }
-                else if (progress>25 && progress<=50)
-                {
+                } else if (progress > 25 && progress <= 50) {
                     textView.setText("Little")
-                }
-                else if(progress>50 && progress<=75)
-                {
+                } else if (progress > 50 && progress <= 75) {
                     textView.setText("Very")
-                }
-                else if (progress>75 && progress<=100)
-                {
+                } else if (progress > 75 && progress <= 100) {
                     textView.setText("Mucho")
                     userChoices[4] = "shot"
                 }
             }
+
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
                 Toast.makeText(applicationContext, "Start tracking", Toast.LENGTH_SHORT).show()
             }
@@ -217,8 +226,6 @@ class PreferenceActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, "Stop tracking", Toast.LENGTH_SHORT).show()
             }
         })
-
-
 
 
     }
